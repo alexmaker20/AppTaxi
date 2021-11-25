8650075669316d8b585f14dbb5d4c6fab6f1c1b9
alexmaker20 <oleksadr0120@gmail.com>
2021-11-25 22:25:32 +0200

    designed and created db

---
+++ Models/Order.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public enum status
+    {
+        NOTAPPOINTED,
+        APPOINTED
+    }
+
+    public class Order
+    {
+        private int passengersAmount;
+
+        public Order() { }
+
+        public Order(string Where, string Destination, int PassengersAmount, status Status, string CarNumber)
+        {
+            this.Where = Where;
+            this.Destination = Destination;
+            this.PassengersAmount = PassengersAmount;
+            this.Status = Status;
+            this.CarNumber = CarNumber;
+        }
+
+        public string Where { get; set; }
+        public string Destination { get; set; }
+        public int PassengersAmount
+        {
+            get
+            {
+                return passengersAmount;
+            }
+            set
+            {
+                if (value < 0 || value > 8)
+                {
+                    throw new ArgumentOutOfRangeException("Wrong amount of passengers");
+                }
+                else
+                {
+                    passengersAmount = value;
+                }
+            }
+        }
+        public status Status { get; set; }
+        public string CarNumber { get; set; }
+    }
+}
