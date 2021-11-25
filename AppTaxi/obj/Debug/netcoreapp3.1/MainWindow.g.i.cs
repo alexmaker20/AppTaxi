6b0a19f07a681889b840914ea872369e0e40192a
alexmaker20 <oleksadr0120@gmail.com>
2021-11-25 22:16:57 +0200

    added project with tests and models

--- AppTaxi/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ AppTaxi/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -63,7 +63,7 @@ namespace AppTaxi {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AppTaxi;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AppTaxi;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
