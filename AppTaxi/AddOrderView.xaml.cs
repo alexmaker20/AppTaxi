ad2b7cd4eef46225fad402ecde4d3a2db872c621
Lolimkeri <demobor96@gmail.com>
2021-11-28 05:53:00 +0200

    Added order and taxist view

---
+++ AppTaxi/AddOrderView.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace OperatorTaxi
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+    using Models;
+
+    /// <summary>
+    /// Логика взаимодействия для AddOrderView.xaml
+    /// </summary>
+    public partial class AddOrderView : Window
+    {
+        public AddOrderView()
+        {
+            InitializeComponent();
+        }
+
+        private void AddOrder(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = (MainWindow)App.Current.MainWindow;
+            if (this.where.Text != "" && this.destination.Text != "" && this.passengersAmount.Text != "")
+            {
+                try
+                {
+                    mainWindow.vm.Orders.Add(new Order(this.where.Text, this.destination.Text, Convert.ToInt32(this.passengersAmount.Text), status.NOTAPPOINTED, "------"));
+                    this.Close();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Incorrect input data!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter all data!");
+            }
+        }
+    }
+}
---
+++ AppTaxi/AddTaxistView.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace OperatorTaxi
+{
+    using System;
+    using System.Windows;
+    using Models;
+
+    /// <summary>
+    /// Логика взаимодействия для AddTaxistView.xaml
+    /// </summary>
+    public partial class AddTaxistView : Window
+    {
+        public AddTaxistView()
+        {
+            InitializeComponent();
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = (MainWindow)App.Current.MainWindow;
+            if (this.Model.Text != "" && this.Number.Text != "")
+            {
+                try
+                {
+                    mainWindow.vm.Taxists.Add(new Taxist(this.Model.Text, this.Number.Text, false));
+                    this.Close();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Incorrect input data!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter all data!");
+            }
+        }
+    }
+}
